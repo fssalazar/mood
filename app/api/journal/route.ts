310c36cd02bcf2f99bb00574859f349d1b5c27b9
fssalazar <salazarfelipe.fs@gmail.com>
2023-07-03 15:16:02 -0300

    feat: analyse entry using chatgpt

---
+++ app/api/journal/route.ts
@@ -0,0 +1,30 @@
+import { analyze } from '@/utils/ai'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+import { revalidatePath } from 'next/cache'
+import { NextResponse } from 'next/server'
+
+export const POST = async () => {
+  const user = await getUserByClerkId()
+  const entry = await prisma.journalEntry.create({
+    data: {
+      userId: user.id,
+      content: 'Write about your day!',
+    },
+  })
+
+  console.log(entry)
+
+  const analysis = await analyze(entry.content)
+  await prisma.analysis.create({
+    data: {
+      entryId: entry.id,
+      ...analysis,
+    },
+  })
+  console.log(entry)
+
+  revalidatePath('/journal')
+
+  return NextResponse.json({ data: entry })
+}
